ba1f747eb4acac3d6d4241da63b1dc3545829176
Elvis-785 <kiruielvis3358@gmail.com>
2025-03-10 17:31:40 +0300

    updated

--- server/storage.js
+++ server/storage.js
@@ -1,33 +1,3 @@
-// // Assuming you have a shared schema (users, User, InsertUser) already defined elsewhere
-
-// class MemStorage {
-//   constructor() {
-//     this.users = new Map();
-//     this.currentId = 1;
-//   }
-
-//   async getUser(id) {
-//     return this.users.get(id);
-//   }
-
-//   async getUserByUsername(username) {
-//     return Array.from(this.users.values()).find(user => user.username === username);
-//   }
-
-//   async createUser(insertUser) {
-//     const id = this.currentId++;
-//     const user = { ...insertUser, id };
-//     this.users.set(id, user);
-//     return user;
-//   }
-// }
-
-// const storage = new MemStorage();
-
-// // Export if you need it for other parts of your application
-// export { storage };
-
-
 class MemStorage {
   constructor() {
     this.users = new Map();
